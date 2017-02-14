2ae8a98982157869ab9080745e71a720b113efef
johnlenon <johnlenonmaghanoy@gmail.com>
2017-02-14 19:54:56 +0800

    Updated webpack configuration.

--- webpack.config.js
+++ webpack.config.js
@@ -6,31 +6,30 @@ const UglifyJsPlugin = webpack.optimize.UglifyJsPlugin;
 module.exports = {
 	"entry": "./falzy.support.js",
 	"resolve": {
-			"descriptionFiles": [
-				".bower.json",
-				"bower.json",
-				"package.json"
-			],
-			"modules": [
-				"bower_components",
-				"node_modules"
-			],
-			"mainFields": [
-				"support",
-				"browser",
-				"module",
-				"main"
-			]
+		"descriptionFiles": [
+			"bower.json",
+			"package.json"
+		],
+		"modules": [
+			"bower_components",
+			"node_modules"
+		],
+		"mainFields": [
+			"support",
+			"browser",
+			"module",
+			"main"
+		]
 	},
-	"module": {
-			"rules": [
-				{
-					"enforce": "pre",
-					"test": /\.support\.js$/,
-					"loader": "source-map-loader"
-				}
-			]
-	},
+	// "module": {
+	// 	"rules": [
+	// 		{
+	// 			"enforce": "pre",
+	// 			"test": /\.support\.js$/,
+	// 			"loader": "source-map-loader"
+	// 		}
+	// 	]
+	// },
 	"output": {
 		"library": "falzy",
 		"libraryTarget": "umd",
@@ -38,15 +37,15 @@ module.exports = {
 	},
 	"plugins": [
 		new UglifyJsPlugin( {
-				"compress": {
-						"keep_fargs": true,
-						"keep_fnames": true,
-						"warnings": false
-				},
-				"comments": false,
-				"sourceMap": true,
-				"mangle": false
+			"compress": {
+				"keep_fargs": true,
+				"keep_fnames": true,
+				"warnings": false
+			},
+			"comments": false,
+			"sourceMap": true,
+			"mangle": false
 		} )
 	],
-	"devtool": "#inline-source-map"
+	"devtool": "#cheap-module-inline-source-map"
 };
